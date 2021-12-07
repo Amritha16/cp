e2cfcb730eb7faa2d006ade4e9162b3afe3ff49d
Amritha <44018437+Amritha16@users.noreply.github.com>
2021-12-07 10:58:40 +0530

    created template.cpp

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> V;
+typedef pair<ll, ll> P;
+
+
+#define MOD 1000000007
+#define INF 1e18
+#define PI 3.141592653589
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define repr(i,a,b) for (int i = a; i > b; i++)
+#define en '\n'
+#define all(v) v.begin(), v.end()
+#define sq(a) (a)*(a)
+
+
+int main() {
+    ll t;
+    cin >> t;
+    while (t--) {
+
+    }
+    return 0;
+}   
